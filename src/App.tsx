f14dab1c75695a586acebbedb24efae69eef20ec
Denis <eledenis@hotmail.com>
2024-11-19 11:03:20 -0300

    1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,211 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { IoMdArrowForward } from "react-icons/io";
+
+import Heart from './assets/heart.png'
+import Wave from './assets/wave.png'
+import PT1_IMG1 from './assets/parte-1/IMG-1.jpg'
+import PT1_IMG2 from './assets/parte-1/IMG-2.jpg'
+import PT1_IMG3 from './assets/parte-1/IMG-3.jpg'
+import PT1_IMG4 from './assets/parte-1/IMG-4.jpg'
+import PT1_IMG5 from './assets/parte-1/IMG-5.jpg'
+import PT1_IMG6 from './assets/parte-1/IMG-6.jpg'
+
+import PT2_IMG0 from './assets/parte-2/IMG-0.jpg';
+import PT2_IMG1 from './assets/parte-2/IMG-1.jpg';
+import PT2_IMG2 from './assets/parte-2/IMG-2.jpg';
+import PT2_IMG3 from './assets/parte-2/IMG-3.jpg';
+import PT2_IMG4 from './assets/parte-2/IMG-4.jpg';
+import PT2_IMG5 from './assets/parte-2/IMG-5.jpg';
+import PT2_IMG6 from './assets/parte-2/IMG-6.jpg';
+import PT2_IMG7 from './assets/parte-2/IMG-7.jpg';
+
+import { Container, Flipper } from './styles';
+
+type PropsMensagem = {
+  id: number,
+  img: string[],
+  text: string
+}
 
 function App() {
+
+  const [home ,setHome] = useState(true);
+  const [parte1 ,setParte1] = useState(false);
+  const [parte2 ,setParte2] = useState(false);
+  const [pts ,setPTS] = useState(0);
+
+  const [mensagem ,setMensagem] = useState(false);
+  const [troca ,setTroca] = useState(false);
+  const [contador ,setContador] = useState<number[]>([]);
+  const [mensgemPricipal, setMensagemPricipal] = useState<PropsMensagem>({img: [], text: '', id: -1});
+
+  const mensagens = [
+    {
+      id: 0,
+      img: [PT1_IMG1],
+      text: "Apesar de tudo e de não parecer, sua família te ama!"
+    },
+    {
+      id: 1,
+      img: [PT1_IMG2, PT1_IMG3],
+      text: "Seus sublinhos te amam muito Tia Laine, Uaine e Ainde. A tia mais divertida do mundo."
+    },
+    {
+      id: 2,
+      img: [PT1_IMG4],
+      text: `A pessoa mais carinhosa e cuidadosa que alguem pode conhecer, 
+      sempre olhando para as pessoa com um brilho no olhar`
+    },
+    {
+      id: 3,
+      img: [PT1_IMG5, PT1_IMG6],
+      text: `A pessoa mais divertida que vou conhecer na minha vida, 
+      meu único motivo sincero de sorrir e dar risada.`
+    }
+  ]
+
+  function Transicao1() {
+    setHome(false);
+    setParte1(true);
+  }
+  function Transicao2() {
+    if (parte1) {
+      setParte1(false);
+      setParte2(true);
+    }else{
+      if (pts > 2) {
+        setContador([]);
+      }
+      else{
+        setPTS(pts+1);
+      }
+    }
+  }
+
+  function SetarMS(item:PropsMensagem, number: number) {
+    if (!contador.includes(number)) {
+      setContador([...contador, number]);
+    }
+    setTroca(true);
+    setTimeout(() => {
+      setMensagemPricipal(item);
+    }, 500);
+    setTimeout(() => {
+      setTroca(false);
+    }, 1500);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {home ? null : 
+        <div className='background'>
+          <img src={Wave} alt="wave" />
+        </div>
+      }
+      {pts === 2 ? null: 
+        <button className={`next ${contador.length === 4 ? 'on' : ''}`} onClick={() => Transicao2()}>
+          <IoMdArrowForward color='#fff' size={28} />
+        </button>
+      }
+      <div className={`home ${home ? "on" : "off"}`}>
+        <button onClick={() => Transicao1()}>
+          <img src={Heart} alt="heart"
+          className='heart-home' />
+        </button>
+        <p>Click no coração</p>
+      </div>
+
+      <div className={`parte-1 ${parte1 ? "on" : "off"}`}>
+        <div className={`mensagens ${mensagem? "on" : "off"} ${troca? 'troca' : ''}`} style={{ maxWidth: mensgemPricipal.img.length > 1 ? 600 : 400 }}>
+          <div className="imgs">
+            {mensgemPricipal.img.map((item, index) => (
+              <img src={item} alt="img1" style={{ objectPosition: index === 1? 'top' : ''}} />
+            ))}
+          </div>
+          <p>{mensgemPricipal.text}</p>
+        </div>
+        <div className={`hearts ${mensagem? "on" : "off"} ${parte1 ? "anime" : '' }`} onClick={() => setMensagem(true)}>
+            <ul>
+              {mensagens.map((item, index) => (
+                <Flipper onClick={() => SetarMS(item, index)} className="flip-container">
+                    <div className={`flipper ${contador.includes(index) ? "on" : ''}`}>
+                      <div className="front">
+                        <img src={Heart} alt="heart" className='hearts-img'/>           
+                      </div>
+                      <div className="back">
+                        <div className="back-container">
+                          <h1>{index}</h1>
+                        </div>
+                      </div>
+                    </div>
+                </Flipper>
+              ))}
+            </ul>
+        </div>
+      </div>
+
+      <div className={`parte-2 ${parte2 ? "on" : "off"}`}>
+        <div className={`pt1 ${pts === 0 ? 'on': ''}`}>
+          <h1>Mãe,</h1>
+          <p>
+            Você é a melhor pessoa desse mundo, sempre se preocupando com 
+            os outros mais do que com você e colcando a felicidade dos outros 
+            acima da sua, pouca gente faz essas coisas como você.
+          </p>
+        </div>
+        <div className={`pt2 ${pts === 1 ? 'on': ''}`}>
+          <p>
+            Nunca se esqueça...Eu te amo MUITO!!!
+          </p>
+          <p>
+            Minha melhor amiga, minha mãe, minha inspiração e meu motivo de levantar todos os dias e não desistir te tudo.
+          </p>
+          <p>
+            Ah...e não se preocupe serei mais criativo nas próximas lembrancinhas como essa kkkkkk.
+          </p>
+        </div>
+        <div className={`pt-final ${pts === 2 ? 'on': ''}`}>
+
+          <div className='left'>
+            <div className={`foto ${pts === 2 ? "on" : ""}`}>
+              <img src={PT2_IMG0} alt="" />
+            </div>
+            <div className={`foto ${pts === 2 ? "on" : ""}`}>
+              <img src={PT2_IMG1} alt="" />
+            </div>
+            <div className={`foto ${pts === 2 ? "on" : ""}`}>
+              <img src={PT2_IMG2} alt="" />
+            </div>
+          </div>
+
+          <div className={`mid ${pts === 2 ? "on" : ""}`}>
+            <div className="foto">
+              <img src={PT2_IMG6} alt="" />
+            </div>
+            <div className="foto">
+              <img src={PT2_IMG7} alt="" />
+            </div>
+            <h1>TE AMO MOZONAAAA!!!!</h1>
+            <h1>ESPERO QUE GOSTE!&#129392;</h1>
+          </div>
+
+          <div className='right'>
+            <div className={`foto ${pts === 2 ? "on" : ""}`}>
+              <img src={PT2_IMG3} alt="" />
+            </div>
+            <div className={`foto ${pts === 2 ? "on" : ""}`}>
+              <img src={PT2_IMG4} alt="" />
+            </div>
+            <div className={`foto ${pts === 2 ? "on" : ""}`}>
+              <img src={PT2_IMG5} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </Container>
   );
 }
 
---
+++ src/styles.ts
@@ -0,0 +1,583 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    overflow: hidd  en;
+    .background{
+        position: absolute;
+        width: 100%;
+        height: 150%;
+        background: linear-gradient(108deg, #FF007F 0%, #EC41EC 100%);
+        animation: back 1s linear;
+    }
+    .background img{
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+    }
+    @keyframes back{
+        0%{transform: translateY(-1500px)}
+        100%{transform: translateY(0)}
+    }
+    .next{
+        background-color: #000;
+        width: 60px;
+        height: 60px;
+        top: 40%;
+        right: -10%;
+        border: 0;
+        position: absolute;
+        border-radius: 50%;
+        transition: .5s;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        z-index: 999;
+    }
+    .next.on{
+        transform: translateX(-300px);
+    }
+    .home.on{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        gap: 1rem;
+        button{
+            width: 250px;
+            border: 0;
+            cursor: pointer;
+            background-color: transparent;
+        }
+        button img{
+            width: 100%;
+            height: 100%;
+            animation: pulse 1s infinite;
+        }
+        p{
+            color: #000000;
+            font-size: 3.2rem;
+        }
+        @keyframes pulse {
+            0% {
+            -webkit-transform: scale3d(1, 1, 1);
+            transform: scale3d(1, 1, 1);
+            }
+            50% {
+            -webkit-transform: scale3d(1.05, 1.05, 1.05);
+            transform: scale3d(1.05, 1.05, 1.05);
+            }
+            100% {
+            -webkit-transform: scale3d(1, 1, 1);
+            transform: scale3d(1, 1, 1);
+            }
+        } 
+    }
+    .home.off{
+        display: none;
+        opacity: 0;
+        transition: 1s;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        gap: 1rem;
+        button{
+            width: 250px;
+            border: 0;
+            cursor: pointer;
+        }
+        button img{
+            width: 100%;
+            height: 100%;
+            animation: pulse 1s infinite;
+        }
+        p{
+            color: #000000;
+            font-size: 3.2rem;
+        }
+    }
+    .parte-1.on{
+        display: block;
+        position: absolute;
+        bottom: 0;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+        justify-content: center;
+        transition: 1s;
+        transition-delay: 1s;
+        bottom: 0;
+        img.hearts-img{
+            transition: 1s;
+            /* position: absolute; */
+            transition-delay: .5s;
+            width: 100px;
+        }
+        .mensagens{
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+            padding: 2rem 1.4rem 2rem 1.4rem;
+            background-color: #fff;
+            box-shadow: 5px 7px 4px 0px rgba(0, 0, 0, 0.25);
+            margin-top: -15rem;
+            transition: 0;
+            scale: 1;
+            animation: swing .3s;
+            /* animation-delay: .2s; */
+            scale: 0;
+        }
+        .mensagens.on{
+            
+            scale: 1;
+        }
+        .mensagens.on .imgs{
+            display: flex;
+            gap: 1rem;
+        }
+        .mensagens.on .imgs img{
+            width: 100%;
+            height: 270px;
+            object-fit: cover;
+        }
+        .mensagens.on p{
+            color: #000;
+            font-size: 2rem;
+            width: 100%;
+            /* min-width: 300px; */
+            /* max-width: 300px; */
+            text-align: center;
+        }
+        .mensagens.on.troca{
+            animation: bugug 1s ease;
+        }
+        .hearts{
+            display: flex;
+            position: absolute;
+            transition: 1s;
+        }
+
+        .hearts ul{
+            display: flex;
+            gap: 4rem;
+        }
+    
+        .hearts.anime .flip-container{
+            width: 100px;
+            height: 100px;
+            
+            transition: .5s;
+            cursor: pointer;
+            animation: slideUp 1.5s ease-in;
+        }
+        .hearts.anime .flip-container:hover{
+            transform: scale(1.3);
+        }
+        .hearts.anime .flip-container:nth-child(1){
+            animation-delay: .5s ;
+        }
+        .hearts.anime .flip-container:nth-child(2){
+            animation-delay: .7s ;
+        }
+        .hearts.anime .flip-container:nth-child(3){
+            animation-delay: .9s ;
+        }
+        .hearts.anime .flip-container:nth-child(4){
+            animation-delay: 1s ;
+        }
+        @keyframes swing {
+            20% {
+            -webkit-transform: rotate3d(0, 0, 1, 15deg);
+            transform: rotate3d(0, 0, 1, 15deg);
+            }
+            40% {
+            -webkit-transform: rotate3d(0, 0, 1, -10deg);
+            transform: rotate3d(0, 0, 1, -10deg);
+            }
+            60% {
+            -webkit-transform: rotate3d(0, 0, 1, 5deg);
+            transform: rotate3d(0, 0, 1, 5deg);
+            }
+            80% {
+            -webkit-transform: rotate3d(0, 0, 1, -5deg);
+            transform: rotate3d(0, 0, 1, -5deg);
+            }
+            100% {
+            -webkit-transform: rotate3d(0, 0, 1, 0deg);
+            transform: rotate3d(0, 0, 1, 0deg);
+            }
+        }
+        .hearts.on{
+            transform: translateY(200px);
+        }
+        @keyframes slideUp {
+            0%{
+                transform: translateY(400px);
+            }
+            100%{
+                transform: translateY(0);
+            }
+        }
+        @keyframes bugug {
+            0%{
+                transform: translateY(0);
+            }
+            50%{
+                transform: translateY(-700px);
+            }
+            100%{
+                transform: translateY(0);
+            }
+        }
+    }
+    .parte-1.off{
+        opacity: 0;
+        /* display: none; */
+        position: absolute;
+        bottom: -1000px;
+    }
+    .parte-2.on{
+        position: absolute;
+        top: 0;
+        /* background-color: blue; */
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        opacity: 1;
+        /* transition: 1s; */
+        .pt1, .pt2{
+            position: absolute;
+            padding: 0 25%;
+        }
+        .pt1, .pt2 p{
+            font-size: 3.2rem;
+            text-align: center;
+        }
+        .pt1{
+            opacity: 0;
+            transition: .5s;
+        }
+        .pt1.on{
+            opacity: 1;
+        }
+        .pt1 h1{
+            font-size: 6rem;
+            margin-bottom: 4rem;
+        }
+        .pt2{
+            opacity: 0;
+            transition: .5s;
+        }
+        .pt2.on{
+            opacity: 1;
+        }
+        .pt-final.on{
+            flex: 3;
+            display: flex;
+            height: 100%;
+            opacity: 1;
+            transition: .2s;
+            .left, .mid, .right{
+                flex: 1;
+            }
+            .left{
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+            }
+            .right{
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+            }
+            .left .foto:nth-child(1){
+                margin-left: -40rem;
+                transform: rotate(15deg);
+            } 
+            .left .foto.on:nth-child(1){
+                margin-left: 0;
+                transform: rotate(15deg);
+                /* transition: 5s; */
+            }
+
+            .left .foto:nth-child(2){
+                margin-left: -40rem;
+                transform: rotate(7deg);
+                transition-delay: .4s;
+            }
+            .left .foto.on:nth-child(2){
+                margin-left: 0;
+                transform: rotate(7deg);
+            }
+
+            .left .foto:nth-child(3){
+                margin-left: -40rem;
+                transform: rotate(-11deg);
+            }
+            .left .foto.on:nth-child(3){
+                margin-left: 0;
+                transform: rotate(-11deg);
+                transition-delay: .6s;
+            }
+
+            .right .foto:nth-child(1){
+                margin-left: 120%;
+                transform: rotate(-15deg);
+            }
+            .right .foto.on:nth-child(1){
+                margin-left: 43%;
+                transform: rotate(-15deg);
+            }
+
+            .right .foto:nth-child(2){
+                margin-left: 120%;
+                transform: rotate(-7deg);
+            }
+            .right .foto.on:nth-child(2){
+                margin-left: 43%;
+                transform: rotate(-7deg);
+                transition-delay: .4s;
+            }
+
+            .right .foto:nth-child(3){
+                margin-left: 120%;
+                transform: rotate(11deg);
+            }
+            .right .foto.on:nth-child(3){
+                margin-left: 43%;
+                transform: rotate(11deg);
+                transition-delay: .6s;
+            }
+
+            .mid{
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                gap: 2rem;
+                margin-top: -200%;
+                transition: 1s;
+            }
+            .mid.on{
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                gap: 2rem;
+                transition: 1s;
+                margin-top: 0;
+            }
+            .mid.on .foto:nth-child(1){
+            }
+            .mid.on .foto:nth-child(2){
+                /* height: 40%; */
+            }
+            .mid.on .foto:nth-child(2) img{
+                height: 350px;
+            }
+            .foto{
+                display: flex;
+                width: 330px;
+                flex-direction: column;
+                gap: 2rem;
+                padding: 2rem 1.4rem 3rem 1.4rem;
+                background-color: #fff;
+                box-shadow: 5px 7px 4px 0px rgba(0, 0, 0, 0.25);
+                transition: 1s;
+                transition-delay: .5s;
+            }
+            .foto img{
+                width: 100%;
+                height: 250px;
+                object-fit: cover;
+            }
+        }
+        .pt-final{
+            opacity: 0;
+            flex: 3;
+            display: flex;
+            height: 100%;
+            /* opacity: 1; */
+            /* transition: 1s; */
+            .left .foto:nth-child(1){
+                margin-left: -40rem;
+                transform: rotate(15deg);
+                transition: 1s;
+            } 
+            .left .foto.on:nth-child(1){
+                margin-left: 0;
+                transform: rotate(15deg);
+                /* transition: 5s; */
+            }
+
+            .left .foto:nth-child(2){
+                margin-left: -40rem;
+                transform: rotate(7deg);
+                transition-delay: .4s;
+            }
+            .left .foto.on:nth-child(2){
+                margin-left: 0;
+                transform: rotate(7deg);
+            }
+
+            .left .foto:nth-child(3){
+                margin-left: -40rem;
+                transform: rotate(-11deg);
+            }
+            .left .foto.on:nth-child(3){
+                margin-left: 0;
+                transform: rotate(-11deg);
+                transition-delay: .6s;
+            }
+
+            .right .foto:nth-child(1){
+                margin-left: 120%;
+                transform: rotate(-15deg);
+            }
+            .right .foto.on:nth-child(1){
+                margin-left: 43%;
+                transform: rotate(-15deg);
+            }
+
+            .right .foto:nth-child(2){
+                margin-left: 120%;
+                transform: rotate(-7deg);
+            }
+            .right .foto.on:nth-child(2){
+                margin-left: 43%;
+                transform: rotate(-7deg);
+                transition-delay: .4s;
+            }
+
+            .right .foto:nth-child(3){
+                margin-left: 120%;
+                transform: rotate(11deg);
+            }
+            .right .foto.on:nth-child(3){
+                margin-left: 43%;
+                transform: rotate(11deg);
+                transition-delay: .6s;
+            }
+
+            .mid{
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                gap: 2rem;
+                margin-top: -200%;
+                transition: 1s;
+            }
+            .mid.on{
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                gap: 2rem;
+                transition: 1s;
+                margin-top: 0;
+            }
+            .mid.on .foto:nth-child(1){
+            }
+            .mid.on .foto:nth-child(2){
+                /* height: 200px; */
+            }
+            .mid.on .foto:nth-child(2) img{
+                height: 100%;
+            }
+            .foto{
+                display: flex;
+                width: 230px;
+                flex-direction: column;
+                gap: 2rem;
+                padding: 2rem 1.4rem 3rem 1.4rem;
+                background-color: #fff;
+                box-shadow: 5px 7px 4px 0px rgba(0, 0, 0, 0.25);
+                transition: 1s;
+            }
+            .foto img{
+                width: 100%;
+                height: 150px;
+                object-fit: cover;
+            }
+            
+            
+        }
+    }
+    .parte-2.off{
+        display: none;
+        opacity: 1;
+        .pt1, .pt2{
+            position: absolute;
+            padding: 0 25%;
+        }
+    }
+`;
+
+export const Flipper = styled.li`
+    perspective: 1000px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    .flipper{
+        position: relative;
+        width: 100%;
+        height: 100%;
+        transition: transform 0.4s;
+            transform-style: preserve-3d;
+    }
+
+    .front, 
+    .back{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        backface-visibility: hidden;
+        display: flex;
+        transition: .5s;
+    }
+
+    .flipper.on{
+        transform: rotateY(180deg);
+    }
+
+    .flipper .front img{
+        height: 100%;
+    }
+    
+    .back{
+	    transform: rotateY(180deg);
+    }
+
+    .flipper .back{
+        display: flex;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+        justify-content: center;
+    }
+    .flipper .back .back-container{
+        /* background-color: red; */
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        border: 2px solid;
+        width: 70%;
+        height: 70%;
+    }
+    .flipper .back .back-container h1{
+        font-size: 2.8rem;
+    }
+    
+`
